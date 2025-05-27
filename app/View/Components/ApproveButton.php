8f2fe4d9a930209899d88b13a8b1c8d2f3f310bf
Jonas Kažukalovas <jonas.kazukalovas@stud.vilniustech.lt>
2025-05-27 13:04:01 +0300

    Refactored code.

--- app/View/Components/ApproveButton.php
+++ app/View/Components/ApproveButton.php
@@ -6,16 +6,8 @@ use Closure;
 use Illuminate\Contracts\View\View;
 use Illuminate\View\Component;
 
-class approveButton extends Component
+class ApproveButton extends Component
 {
-    /**
-     * Create a new component instance.
-     */
-    public function __construct()
-    {
-        //
-    }
-
     /**
      * Get the view / contents that represent the component.
      */
--- app/View/Components/DenyButton.php
+++ app/View/Components/DenyButton.php
@@ -6,16 +6,8 @@ use Closure;
 use Illuminate\Contracts\View\View;
 use Illuminate\View\Component;
 
-class denyButton extends Component
+class DenyButton extends Component
 {
-    /**
-     * Create a new component instance.
-     */
-    public function __construct()
-    {
-        //
-    }
-
     /**
      * Get the view / contents that represent the component.
      */
